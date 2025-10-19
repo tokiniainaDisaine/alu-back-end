8ead70ccd04226bd32f6f36e25795107f2f6602a
TheRealToky <tokydisaine.andrianarison@gmail.com>
2025-10-19 22:08:13 +0200

    fix 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,20 +15,27 @@ def main(employee_id):
     """
     fgbjfnd
     """
-    employee_info_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
-    employee_todo_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    employee_info_url = \
+    f"https://jsonplaceholder.typicode.com/users/{employee_id}/"
+
+    employee_todo_url = \
+    f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
     employee_info = get_info(employee_info_url)
     employee_todo = get_info(employee_todo_url)
 
     employee_name = employee_info.get("name")
-    tasks = {todo.get("title"): todo.get("completed") for todo in employee_todo}
+    tasks = {todo.get("title"): \
+             todo.get("completed") for todo in employee_todo}
 
     task_number = len(tasks)
-    completed_tasks = [completed for completed in tasks.values() if completed]
+    completed_tasks = [completed \
+                       for completed in tasks.values() if completed]
     completed_tasks_count = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({completed_tasks_count}/{task_number}):")
+    print(f"Employee {employee_name} is done with tasks\
+({completed_tasks_count}/{task_number}):")
+    
     for title, completed in tasks.items():
         if completed:
             print(f"\t {title}")
